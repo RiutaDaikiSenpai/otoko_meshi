aff6ec47c3edc99fca2e00f8e235361b6d5a3cc3
RiutaDaikiSenpai <daikiriuta0926@gmail.com>
2023-07-02 01:40:53 +0900

    記録画面にウィジェット配置

--- lib/screens/write.dart
+++ lib/screens/write.dart
@@ -17,8 +17,86 @@ class WriteScreen extends StatelessWidget {
       ),
       body: Container(
           color: Colors.white,
-          child: const Center(
-              child: Text('記す', style: TextStyle(fontSize: 32.0)))),
+          child: Center(
+              child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(10)),
+                child: const SizedBox(
+                  height: 130,
+                  width: 260,
+                ),
+              ),
+              const SizedBox(
+                width: 350,
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      hintText: '料理名',
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black))),
+                ),
+              ),
+              const SizedBox(
+                width: 350,
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      hintText: '材料',
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black))),
+                ),
+              ),
+              const SizedBox(
+                width: 350,
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      hintText: '手順1',
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black))),
+                ),
+              ),
+              const SizedBox(
+                width: 350,
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      hintText: '手順2',
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black))),
+                ),
+              ),
+              const SizedBox(
+                width: 350,
+                child: TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      hintText: '手順3',
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black))),
+                ),
+              ),
+
+              Row(
+                children: const [
+                  ElevatedButton(onPressed: null, child: Text('リセット')),
+                  ElevatedButton(onPressed: null, child: Text('メモして投稿')),
+                  ElevatedButton(onPressed: null, child: Text('メモする'))
+                ],
+              )
+            ],
+          ))),
     );
   }
 }
